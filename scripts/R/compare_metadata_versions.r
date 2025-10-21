90a538f417b90b0f3e8e3b3e40fd212a01c3ee83
kirstyn <kirstyn@users.noreply.github.com>
2025-10-21 10:17:23 +0100

    Update compare_metadata_versions.r

--- scripts/R/compare_metadata_versions.r
+++ scripts/R/compare_metadata_versions.r
@@ -7,11 +7,11 @@ library(compareDF)
 #https://github.com/alexsanjoseph/compareDF
 
 #df1
-old=read.csv("processed_data/processed_metadata/gathered_metadata_n797_20251020_154332.csv")
+old=read.csv("processed_data/processed_metadata/gathered_metadata_n797_20251020_161614.csv")
 
 
 #df2
-new=read.csv("processed_data/processed_metadata/gathered_metadata_n797_raddlCorrected_20251020_154332.csv")
+new=read.csv("processed_data/processed_metadata/gathered_metadata_n797_raddlCorrected_20251020_161614.csv")
 
 # do the comparison
 comparedf=compare_df(new, old, c("Sample_ID"))
@@ -21,5 +21,5 @@ comparedf=compare_df(new, old, c("Sample_ID"))
 #create_output_table(comparedf)
 # for excel output
 #create_output_table(c1, output_type = 'xlsx', file_name = "outputs/discrepancies_15feb.xlsx")
-create_output_table(comparedf, output_type = 'xlsx', file_name = "processed_data/processed_metadata/gathered_metadata_n797_compare.xls")
+create_output_table(comparedf, output_type = 'xlsx', file_name = "processed_data/processed_metadata/gathered_metadata_n797_compare.xlsx")
 
