1be1c947c431d2fb81674536601b35f64197c0eb
kirstyn <kirstyn@users.noreply.github.com>
2025-10-22 12:10:26 +0100

    Update compare_metadata_versions.r

--- scripts/R/compare_metadata_versions.r
+++ scripts/R/compare_metadata_versions.r
@@ -4,22 +4,46 @@
 #devtools::install_github('alexsanjoseph/compareDF')
 
 library(compareDF)
-#https://github.com/alexsanjoseph/compareDF
-
-#df1
-old=read.csv("processed_data/processed_metadata/gathered_metadata_n797_20251020_161614.csv")
 
 
-#df2
-new=read.csv("processed_data/processed_metadata/gathered_metadata_n797_raddlCorrected_20251020_161614.csv")
+# import two datasets to compare (I call "new" the one with latest updates)
+old=read.csv("processed_data/processed_metadata/gathered_metadata_n797_20251021_125349.csv")
+new=read.csv("processed_data/processed_metadata/gathered_metadata_n797_raddl_and_manual_Corrected_20251021_125349.csv")
 
-# do the comparison
+
+# Compare whole dataset: 
+
+# do the comparison (group by Sample id, i.e. it compares the info for each sample id)
 comparedf=compare_df(new, old, c("Sample_ID"))
 
-# summarise
-# for html (in viewer):
-#create_output_table(comparedf)
-# for excel output
-#create_output_table(c1, output_type = 'xlsx', file_name = "outputs/discrepancies_15feb.xlsx")
-create_output_table(comparedf, output_type = 'xlsx', file_name = "processed_data/processed_metadata/gathered_metadata_n797_compare.xlsx")
 
+# summarise
+
+# for html (in R viewer):
+#create_output_table(comparedf)
+
+# for excel output
+create_output_table(comparedf, output_type = 'xlsx', file_name = "processed_data/processed_metadata/gathered_metadata_n797_confirmManualCorrections.xlsx")
+
+
+#Compare specific columns 
+
+library(compareDF)
+
+# Columns to compare
+cols_to_compare <- c("Sample_ID", "Preferred_date")
+
+# Subset the dataframes to only these columns
+old_sub <- old[, cols_to_compare]
+new_sub <- new[, cols_to_compare]
+
+# Compare, matching on Sample_ID
+comparedf <- compare_df(
+  df_new = new_sub,
+  df_old = old_sub,
+  group_col = "Sample_ID"
+)
+
+# Export Excel
+create_output_table(comparedf, output_type = "xlsx",
+                    file_name = "processed_data/processed_metadata/gathered_metadata_n797_confirmManualCorrections.xlsx")
