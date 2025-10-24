f6418346edcf16a45b703385cadb0aa4cbd437d7
kirstyn <kirstyn@users.noreply.github.com>
2025-10-24 11:30:29 +0100

    Update redcap-matchncbidata.R

--- scripts/R/redcap-matchncbidata.R
+++ scripts/R/redcap-matchncbidata.R
@@ -2,16 +2,14 @@
 #- identify any missing records in redcap
 #- filter the ncbi data to only additional data (i.e not ours)
 library(dplyr)
+library(stringr)
 
 ## ncbi data
-ncbi=read.csv("/Users/kirstyn.brunker/philippines/files_received/fasta_fromNCBI_15jul2025/metadata_coverage_90_country_Philippines.csv")
+ncbi=read.csv("raw_data/vgtk_ncbi_data/150725_metadata_coverage_90_country_Philippines.csv")
 
 # latest redcap data
-redcap=read.csv("/Users/kirstyn.brunker/GitHub/rage-redcap-developer/philippines/processed_data/redcap_sequences_and_metadata/redcap_download_20250814_114620250814_1146redcap_meta_phl.csv")
+redcap_seq=read.csv("raw_data/redcap_sequences_and_metadata/redcap_download_20251023_134520251023_1345redcap_meta_phl.csv")
 
-# filter to only sequencing rows
-redcap_seq=redcap %>%
-  filter(redcap_repeat_instrument=="sequencing")
 head(redcap_seq)
 head(ncbi)
 
@@ -46,7 +44,7 @@ redcap_updated <- redcap_seq %>%
     )
   ) %>%
   select(-primary_accession)  # remove helper column
-write.csv(redcap_updated, "/Users/kirstyn.brunker/philippines/redcap_imports/130825_genbankAccessionUpdates.csv" ,row.names=F)
+write.csv(redcap_updated, "redcap_imports/130825_genbankAccessionUpdates.csv" ,row.names=F)
 
 
 
@@ -55,3 +53,24 @@ ncbi.only=ncbi.team %>%
   filter(!isolate %in% redcap$sample_id)
 ncbi.only$isolate
 #[1] "Z15-185" "Z14-142" "Z14-152" "Z12-012" : problem related to sample id typos (missing hypen)
+
+
+# Filter out all records submitted by your team
+ncbi.additional <- ncbi %>%
+  filter(!str_detect(authors, regex("brunker", ignore_case = TRUE)))
+
+# Write to CSV
+write.csv(ncbi.additional, "redcap_imports/ncbi_additional_data_excl_brunker.csv", row.names = FALSE)
+
+# Read the NCBI sequences
+ncbi.seq <- read.fasta("raw_data/vgtk_ncbi_data/150725_metadata_coverage_90_country_Philippines_sequences.fa", seqtype = "DNA", as.string = TRUE)
+
+# Keep only sequences that are in ncbi.additional
+# Assuming sequence names in FASTA match `isolate` column
+seq.names.to.keep <- ncbi.additional$isolate
+ncbi.seq.filtered <- ncbi.seq[names(ncbi.seq) %in% seq.names.to.keep]
+
+# Write filtered sequences to new FASTA
+write.fasta(sequences = ncbi.seq.filtered, 
+            names = names(ncbi.seq.filtered), 
+            file.out = "redcap_imports/ncbi_additional_sequences_excl_brunker.fa")
